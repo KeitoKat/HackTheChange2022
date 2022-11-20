e1393fcc371852108dc88108eee6458ae7fe9437
ArmanDris <adrismir@gmail.com>
2022-11-20 00:14:20 -0800

    Moved bulk of code to homepage directory

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,35 @@
-import logo from './logo.svg';
-import heart from './heart.png';
 import './App.css';
-import Header from './Header.js';
-import Logo from './Logo.js';
+import Homepage from './Homepage/Homepage.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <div id="Icons">
-          <Logo></Logo>
-          <Logo></Logo>
-          <Logo></Logo>
-        </div>
-      </nav>
-      <header className="App-header">
-        <table>
-          <tr>
-            <td colspan='3'><b>Welcome!</b> Are you a</td>
-          </tr>
-          <tr>
-            <td><button>guest</button></td>
-            <td><p> or a </p></td>
-            <td><button>host</button></td>
-          </tr>
-        </table>
-      </header>
+    <div>
+      <Homepage/>
     </div>
+
+    // <div className="App">
+    //   <nav>
+    //     <div id="Icons">
+    //       <Logo></Logo>
+    //       <Logo></Logo>
+    //       <Logo></Logo>
+    //     </div>
+    //   </nav>
+    //   <header className="App-header">
+    //     <table>
+    //       <tr>
+    //         <td colspan='3'><b>Welcome!</b> Are you a</td>
+    //       </tr>
+    //       <tr>
+    //         <td><button>guest</button></td>
+    //         <td><p> or a </p></td>
+    //         <td><button>host</button></td>
+    //       </tr>
+    //     </table>
+    //   </header>
+    // </div>
+
   );
 }
 
